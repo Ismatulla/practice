34be1269ffadd753d94d9dbc1ff70e860ff17d83
Ismatulla <ismatilloqoyliyev@gmail.com>
2023-06-19 07:18:02 +0200

    algo-challenge

--- 2point.js
+++ 2point.js
@@ -1,20 +1,56 @@
 // 2 point solution ,time complexity O(n)
-function countUniqueValues(arr) {
-  if (arr.length === 0) {
-    return
+// function averagePair(arr, avg) {
+//   if (arr.length === 0) {
+//     return false
+//   }
+//   let point1 = 0;
+//   let point2 = arr.length - 1
+//   while (point1 < arr.length) {
+//     if (((arr[point1] + arr[point2]) / 2) === avg) {
+//       return true
+//     } else if (((arr[point1] + arr[point2]) / 2) < avg) point1++
+//     else point2--
+//   }
+//   return false
+// }
+// console.log(averagePair([1, 2, 3, 5], 4.5))
+
+
+// find subsequent chars time complexity O(m+n)
+
+// function isSubsiquent(str1, str2) {
+//   let i = 0;
+//   let j = 0;
+//   if (!str1) return true
+//   while (j < str2.length) {
+//     if (str2[j] === str1[i]) i++
+//     if (i === str1.length) return true
+//     j++
+//   }
+//   return false
+// }
+
+// console.log(isSubsiquent('hello', 'hlelo world'))
+
+// find max sum of subarray consecetive order 
+
+function maxSubarraySum(arr, num) {
+  if (arr.length < num) return null
+  let total = 0
+  for (let i = 0; i < num; i++) {
+    total += arr[i]
   }
-  let point1 = 0
-  let point2 = 1
-  while (point2 < arr.length) {
-    if (arr[point1] === arr[point2]) {
-      point2++
-    } else if (arr[point1] !== arr[point2]) {
-      point1++
-      arr[point1] = arr[point2]
-    }
+  let current = total
+  for (let i = num; i < arr.length; i++) {
+    current += arr[i] - arr[i - num];
+    console.log(arr[i - num])
+    total = Math.max(total, current);
   }
-  return point1 + 1
+  return total
 }
 
-console.log(countUniqueValues([1, 1, 1, 2, 3, 4, 4, 7]))
-
+maxSubarraySum([100, 200, 300, 400], 2) // 700
+// maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)  // 39 
+// maxSubarraySum([-3, 4, 0, -2, 6, -1], 2) // 5
+// maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2) // 5
+// maxSubarraySum([2, 3], 3) // null
--- slidingWindow.js
+++ slidingWindow.js
@@ -18,26 +18,64 @@
 
 // console.log(maxSubarraySum([1, 2, 4, 5, 6, 9, 10, 3, 5, 6, 12], 5))
 
-function sameFrequency(num1, num2) {
-  // good luck. Add any arguments you deem necessary.
-  let str1 = String(num1);
-  let str2 = String(num2);
+// function sameFrequency(num1, num2) {
+//   // good luck. Add any arguments you deem necessary.
+//   let str1 = String(num1);
+//   let str2 = String(num2);
 
-  if (str1.length !== str2.length) {
-    return false;
-  }
-  let lookup = {}
-  for (let i = 0; i < str1.length; i++) {
-    let letters = str1[i]
-    lookup[letters] ? lookup[letters] += 1 : lookup[letters] = 1
-  }
-  for (let i = 0; i < str2.length; i++) {
-    let letters = str2[i]
-    if (!lookup[letters]) {
-      return false
-    } else {
-      lookup[letters] -= 1
+//   if (str1.length !== str2.length) {
+//     return false;
+//   }
+//   let lookup = {}
+//   for (let i = 0; i < str1.length; i++) {
+//     let letters = str1[i]
+//     lookup[letters] ? lookup[letters] += 1 : lookup[letters] = 1
+//   }
+//   for (let i = 0; i < str2.length; i++) {
+//     let letters = str2[i]
+//     if (!lookup[letters]) {
+//       return false
+//     } else {
+//       lookup[letters] -= 1
+//     }
+//   }
+//   return true
+// }
+
+// challenge 3 frequency counter or multiple point solution 
+
+// function areThereDuplicates(...nums) {
+//   // good luck. (supply any arguments you deem necessary.)
+//   let freqCheck = {}
+//   for (let i = 0; i < nums.length; i++) {
+//     let number = nums[i]
+//     freqCheck[number] ? freqCheck[number] += 1 : freqCheck[number] = 1
+//   }
+//   for (let val in freqCheck) {
+//     if (freqCheck[val] !== 1) {
+//       return true
+//     }
+//   }
+//   return false
+// }
+
+// 2 pointers solution
+
+function areThereDuplicates(...nums) {
+  // good luck. (supply any arguments you deem necessary.)
+  let freqCheck = {}
+  for (let i = 0; i < nums.length; i++) {
+    let point1 = nums[i]
+    let point2 = nums[i + 1]
+    if (i + 1 <= nums.length) {
+      point1 === point2 ? freqCheck[point1] += 1 : freqCheck[point1] = 1
     }
   }
-  return true
+  console.log(freqCheck)
 }
+
+// test cases
+areThereDuplicates('a', 'b', 'c', 'a')
+// areThereDuplicates(1, 2, 3) // false
+//areThereDuplicates(1, 2, 2) // true
+//areThereDuplicates('a', 'b', 'c', 'a') // true
