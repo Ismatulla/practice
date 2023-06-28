67395d5c4e937d3b0dd45590b8496188dd429f01
Ismatulla <ismatilloqoyliyev@gmail.com>
2023-06-28 12:59:33 +0200

    quick-sort-practice

--- quicksort.js
+++ quicksort.js
@@ -1,28 +1,31 @@
-function pivot(arr, start = 0, end = arr.lenth - 1) {
-  // swap it
-  const swap = (arr, idx1, idx2) => {
-    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
-  }
+function pivots(arr, start = 0, end = arr.length - 1,) {
+  function swap(array, i, j) {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp
 
-  // We are assuming that pivot is always the first element
-  let pivot = arr[start];
-  let swapIdx = start;
-  for (let i = start + 1; i <= end; i++) {
+  };
+  let pivot = arr[start]
+  let swapIdx = start
+
+  for (var i = start + 1; i < arr.length; i++) {
     if (pivot > arr[i]) {
-      swapIdx++
+      swapIdx++;
       swap(arr, swapIdx, i)
     }
   }
-  swap(arr, start, swapIdx);
+  swap(arr, start, swapIdx)
+  console.log(arr)
   return swapIdx
 }
+
+
 function quickSort(arr, left = 0, right = arr.length - 1) {
   if (left < right) {
-    let pivotIndex = pivot(arr, left, right);
+    let pivotIndex = pivots(arr, left, right)
     // left
-    quickSort(arr, left, pivotIndex - 1)
-    // right
-    quickSort(arr, pivotIndex + 1, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right)
   }
   return arr
 }
